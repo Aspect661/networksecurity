b61a0a8913da62dc2082afc3188fab832c95291b
TuhinDas661 <tuhindas01.official@gmail.com>
2026-06-22 00:50:59 -0700

    Model Evaluation v1

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -16,6 +16,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import (AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier)
 
+import mlflow
+
 class ModelTrainer:
     def __init__(self, model_trainer_config: ModelTrainerConfig, data_transformation_artifact: DataTransformationArtifact):
 
@@ -24,6 +26,16 @@ class ModelTrainer:
             self.data_transformation_artifact = data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e, sys)
+
+    def track_mlflow(self, best_model, classification_metric):
+        with mlflow.start_run():
+            f1_score = classification_metric.f1_score
+            precision_score = classification_metric.precision_score
+            recall_score = classification_metric.recall_score
+
+            mlflow.log_metrics({"f1_score": f1_score, "precision_score": precision_score, "recall_score": recall_score})
+
+            mlflow.sklearn.log_model(best_model, "model")
 
      # Training and evaluating the model   
     def train_model(self, X_train, y_train, X_test, y_test):
@@ -46,12 +58,12 @@ class ModelTrainer:
                 # 'criterion':['gini', 'entropy', 'log_loss'],
                 
                 # 'max_features':['sqrt','log2',None],
-                'n_estimators': [8,16,32,128,256]
+                'n_estimators': [8,16,32,100,128,200,256]
             },
             "Gradient Boosting":{
                 # 'loss':['log_loss', 'exponential'],
                 'learning_rate':[.1,.01,.05,.001],
-                'subsample':[0.6,0.7,0.75,0.85,0.9],
+                'subsample':[0.6,0.65,0.69,0.7,0.75,0.85,0.9],
                 # 'criterion':['squared_error', 'friedman_mse'],
                 # 'max_features':['auto','sqrt','log2'],
                 'n_estimators': [8,16,32,64,128,256]
@@ -83,9 +95,14 @@ class ModelTrainer:
         classification_train_metric = get_classification_score(y_true = y_train, y_pred = y_train_pred)
         
         # Track the experiements with mlflow
+        self.track_mlflow(best_model, classification_train_metric)
+
 
         y_test_pred = best_model.predict(X_test)
         classification_test_metric = get_classification_score(y_true = y_test, y_pred = y_test_pred)
+
+        # Track the experiements with mlflow
+        self.track_mlflow(best_model, classification_test_metric)
 
         preprocessor = load_object(file_path = self.data_transformation_artifact.transformed_object_file_path)
         model_dir_path = os.path.dirname(self.model_trainer_config.trained_model_file_path)
@@ -123,7 +140,9 @@ class ModelTrainer:
             )
 
             # Loading the model
-            model = self.train_model(X_train, y_train, X_test, y_test)
+            model_trainer_artifact = self.train_model(X_train, y_train, X_test, y_test)
+            
+            return model_trainer_artifact
             
         except Exception as e:
             raise NetworkSecurityException(e, sys)
